fd9aeb832e0e958e001e76a9328eeaf64d7c1aff
Joshinobi <gonzalesjosh1999@gmail.com>
2020-11-29 01:42:54 +0800

    Dashboard and other updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-        <h1>home</h1>
+    <div class="home">
     </div>
 </template>
 
@@ -9,3 +8,13 @@ export default {
     name: 'home'
 }
 </script>
+
+<style lang="scss" scoped>
+
+    * {
+        margin: 0;
+        padding: 0;
+        min-height: 720px;
+        background-color: red;
+    }
+</style>
